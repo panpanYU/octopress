f65d0df1e27af3e88061317c898b6e375a6a6146
panpanYU <499432144@qq.com>
2015-05-19 20:27:03 +0800

    修改首页

---
+++ javascripts/js519.js
@@ -0,0 +1,18 @@
+
+
+//var oHide=document.getElementById("hide");
+//oHide.onclick=function fn(){
+//    if(hide_ol.style.display=="block"){
+//        document.getElementById("hide_ol").style.display="none";
+//        oHide.innerHTML=">>>";
+//    }else{
+//        document.getElementById("hide_ol").style.display="block";
+//        oHide.innerHTML="∨";
+//    }
+//}
+//
+
+
+
+
+
